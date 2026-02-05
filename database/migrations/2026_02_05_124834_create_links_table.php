560b214e7db15c1414647d3e588cb8e8f3e652b5
millord1 <roffat.theau@outlook.com>
2026-02-05 13:53:27 +0100

    create Links migration

---
+++ database/migrations/2026_02_05_124834_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('short_code', 10)->unique()->index();
+            $table->string('original_url');
+            $table->integer('clicks_count')->default(0);
+            $table->timestamp('last_used_at')->nullable();
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
