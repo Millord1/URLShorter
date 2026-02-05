e45203eb6c83c10ccc34a2a7e7b1fdfdbd461b84
millord1 <roffat.theau@outlook.com>
2026-02-05 14:10:57 +0100

    create service and fill model

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Link extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'short_code',
+        'original_url',
+        'clicks_count',
+        'last_used_at',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('User');
+    }
+
 }
